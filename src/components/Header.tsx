e57d9bacfa6ccc9b940efb4492b48f3f20f1ff62
Thais Silva <thatsi.fer@gmail.com>
2025-02-03 22:35:08 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+
+
+
+const Header = () => {
+  return (
+    
+        <header>
+          <div className="flex flex-row items-center justify-between">
+            <img className= ' w-[50px] h-8'src='../assets/images/Logo.png' alt="logo" />
+            <h1 className= ' text-black text-[32px] font-bold '>Furniro</h1>
+            
+            
+            <i className="fa-regular fa-user"></i>
+            <i className="fa-light fa-cart-shopping"></i>
+            </div>
+            
+                
+        </header>
+      
+  
+  )
+}
+
+export default Header
