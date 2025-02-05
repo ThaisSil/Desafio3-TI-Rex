2fa080abdb02b80e88afb87c53a9e4579bc32977
Thais Silva <thatsi.fer@gmail.com>
2025-02-04 21:07:49 -0300

    feat: create BrowseSection

---
+++ src/layout/BrowseSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Dining from '../assets/images/Dining.png';
+import Living from '../assets/images/Living.png';
+import Bedroom from '../assets/images/Bedroom.png';
+
+
+const BrowseSection = () => {
+  return (
+    <div className='flex flex-col items-center justify-center py-20 px-32'>
+        <p className='text-[#333333] font-bold text-[32px] '>Browse The Range</p>
+        <p className='text-[#666666] text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className='flex flex-row justify-between gap-5 py-16 mx-32'>
+        <div>
+            <img src={Dining} alt="dining" />
+            <p className='font-semibold text-2xl text-center p-7'>Dining</p>
+        </div>
+        <div>
+            <img src={Living} alt="living" />
+            <p className='font-semibold text-2xl text-center p-7'>Living</p>
+        </div>
+        <div>
+            <img src={Bedroom} alt="bedroom" />
+            <p className='font-semibold text-2xl text-center p-7'>Bedroom</p>
+        </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default BrowseSection
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../components/Header';
 import MainSection from '../layout/MainSection';
 import Footer from '../components/Footer';
+import BrowseSection from '../layout/BrowseSection';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <MainSection />
+      <BrowseSection />
       <Footer />
+      
             
     </div>
   )
