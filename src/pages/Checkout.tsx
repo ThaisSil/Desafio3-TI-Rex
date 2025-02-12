f15e299c7a6cc79ec91c99ee1e527b366d00ae42
Thais Silva <thatsi.fer@gmail.com>
2025-02-12 09:55:14 -0300

    fix: page checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,25 +1,69 @@
-import React from 'react'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
-import BgBanner from '../assets/images/BgBanner.png'
-import Warranty from '../components/Warranty'
-import Footer from '../components/Footer'
-import CheckoutForm from '../components/CheckoutForm'
+import React from "react";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import BgBanner from "../assets/images/BgBanner.png";
+import Warranty from "../components/Warranty";
+import Footer from "../components/Footer";
+import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
   return (
     <div>
       <Header />
-      <Banner 
-      bgImage={BgBanner}
-      title='Checkout '
-      subtitle= 'Home >'
-      paragraph='Checkout'/>
-      <CheckoutForm />
+      <Banner
+        bgImage={BgBanner}
+        title="Checkout "
+        subtitle="Home >"
+        paragraph="Checkout"
+      />
+      <div className="grid grid-cols-2">
+        <CheckoutForm />
+        <div>
+        <div className="grid grid-cols-2">
+          <div className="my-14 p-9">
+            <p className="font-medium text-2xl py-4">Product</p>
+            <p className="text-[#9F9F9F] py-4">
+             nomproduto
+              <span className="text-xs text-black">x quant.prod</span>
+            </p>
+            <p className="py-4"> Subtotal</p>
+            <p className="py-4">Total</p>
+          </div>
+          <div className="my-14 p-16">
+            <p className="font-medium text-2xl py-4">Subtotal</p>
+            <p className="py-4"> preço do produto </p>
+            <p className="py-4"> preço subtotal</p>
+            <p className="font-bold text-2xl text-[#B88E2F] py-4">
+              preço total
+            </p>
+          </div>
+          </div>
+          <div className="grid grid-cols-1 p-9">
+          <p className="py-4">Direct Bank Transfer</p>
+          <p className="text-[#9F9F9F] text-justify text font-light py-4">
+            Make your payment directly into our bank account. Please use your
+            Order ID as the payment reference. Your order will not be shipped
+            until the funds have cleared in our account.
+          </p>
+          <p className="text-[#9F9F9F] text font-medium py-4">
+            Direct Bank Transfer
+          </p>
+          <p className="text-[#9F9F9F] text font-medium py-4">Cash On Delivery</p>
+          <p className="font-light text-justify py-4">
+            Your personal data will be used to support your experience
+            throughout this website, to manage access to your account, and for
+            other purposes described in our <span className="font-semibold">privacy policy.</span>
+          </p>
+          <button className=" text-xl w-[318px] h-[64px] border border-black rounded-2xl mx-36 my-9">Place order</button>
+        </div>
+       </div>
+      </div>
+      
+
       <Warranty />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
